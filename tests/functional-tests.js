77011e6e1729d89e0cb5f86a6e4b70a337854d44
arunnayan <arunnayan.28@gmail.com>
2024-07-03 13:01:02 -0700

    remove node_modules

--- tests/functional-tests.js
+++ tests/functional-tests.js
@@ -1,4 +1,4 @@
-onst { Builder, By, Key, until } = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 const assert = require('assert');
 
 describe('Functional Tests', function() {
